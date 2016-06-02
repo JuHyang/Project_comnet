68be496c9397c74c74f942c1e371398922029c87
JuHyang <kkss2889@naver.com>
2016-06-02 11:02:56 +0900

    2016 06 02 잘못 업로드 한 파일 정리

--- Project_comnet-master/src/Django/Project_comnet/Odello/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, render_to_response
-from django.template import RequestContext
-
-from django.contrib.auth.models import User # 유저모델 가져오기
-from django.contrib.auth import logout
-from django.http import HttpResponseRedirect
-from Project_comnet.forms import *
-# Create your views here.
