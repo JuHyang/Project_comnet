fc1527f8ed0a455acb392eb0acbab33169256d53
JuHyang <kkss2889@naver.com>
2016-06-06 16:58:50 +0900

    2016.06.06 시작

--- src/Django/Project_comnet/login/views.py
+++ src/Django/Project_comnet/login/views.py
@@ -44,6 +44,6 @@ def logout_page(request):
 @login_required
 def home(request):
     return render_to_response(
-        'home.html',
+        'Gamerooms.html',
         {'user': request.user}
     )
